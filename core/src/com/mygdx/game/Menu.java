97e196e2969233cdb4da86dcfe77bd1f36af869d
bigmeco <bigmecoprog@gmail.com>
2017-04-01 18:36:50 +0300

    menu

---
+++ core/src/com/mygdx/game/Menu.java
@@ -0,0 +1,44 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by bigi on 01.04.2017.
+ */
+
+public class Menu implements Screen {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+        //dispose();
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,33 +1,74 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class MyGdxGame extends Game {
+	OrthographicCamera camera;
 	SpriteBatch batch;
 	Texture img;
-	
+	Rectangle rectangle;
+	float x;
+	float g;
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new Menu());
+
+////		camera = new OrthographicCamera();
+////		camera.setToOrtho(false,1280,720);
+//		batch = new SpriteBatch();
+////		rectangle = new Rectangle();
+////		rectangle.x = 1280/2;
+////		rectangle.y = 720/2;
+////		rectangle.width= 128;
+////		rectangle.height= 128;
+//
+//		img = new Texture("badlogic.jpg");
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
+
+//
+//	@Override
+//	public void render () {
+//		Gdx.gl.glClearColor(0, 0, 0, 1);
+//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//		x += Gdx.graphics.getDeltaTime();
+//
+////		camera.update();
+////		batch.setTransformMatrix(camera.combined);
+//		batch.begin();
+//
+//		batch.draw(img,x*20, x*10);
+//		batch.end();
+//	}
+//
+//	@Override
+//	public void dispose () {
+//		batch.dispose();
+//		img.dispose();
+//	}
+//
+//	@Override
+//	public void resize(int width, int height) {
+//		super.resize(1280, 720);
+//		System.out.println(width+"   "+height);
+//
+//	}
+//
+//	@Override
+//	public void pause() {
+//		super.pause();
+//	}
+//
+//	@Override
+//	public void resume() {
+//		super.resume();
+//	}
 }
